0baa00d259c25fcee5c7485734d89d02baf29786
SOHAMPATEL1483 <86504280+SOHAMPATEL1483@users.noreply.github.com>
2023-08-29 19:01:08 +0530

    added comments to move function for better readability

--- chandrayaan.js
+++ chandrayaan.js
@@ -33,6 +33,10 @@ export class Chandrayaan {
     }
   }
 
+  /* 
+  move function will be responsible for moving one step in the direction of chandrayaan
+  we have direction_to_axis object which will convert direction to respective coordinate axis
+   */
   move(command) {
     switch (command) {
       case "f":
@@ -40,18 +44,26 @@ export class Chandrayaan {
           this.direction === "U" ||
           this.direction === "N" ||
           this.direction === "E"
-        )
+        ) {
+          // now if we in North , east or up direction than to move forward we have to +1;
           this[this.#direction_to_axis[this.direction]]++;
-        else this[this.#direction_to_axis[this.direction]]--;
+        } else {
+          // but if we in south , west or down direction than to move forward we have to -1;
+          this[this.#direction_to_axis[this.direction]]--;
+        }
         break;
       case "b":
         if (
           this.direction === "U" ||
           this.direction === "N" ||
           this.direction === "E"
-        )
+        ) {
+          // now if we in North , east or up direction than to move backword we have to -1;
           this[this.#direction_to_axis[this.direction]]--;
-        else this[this.#direction_to_axis[this.direction]]++;
+        } else {
+          // but if we in south , west or down direction than to move forward we have to +1;
+          this[this.#direction_to_axis[this.direction]]++;
+        }
         break;
       default:
         console.error("paramaters passed in move function are wrong");
@@ -59,5 +71,38 @@ export class Chandrayaan {
     }
   }
 
-  turn(command) {}
+  turn(command) {
+    switch (command) {
+      case "l":
+        if (this.direction == "N") this.direction = "W";
+        else if (this.direction == "W") this.direction = "S";
+        else if (this.direction == "S") this.direction = "E";
+        else if (this.direction == "E") this.direction = "N";
+        else if (this.direction == "U") this.direction = "N";
+        else if (this.direction == "D") this.direction = "S";
+        break;
+      case "r":
+        if (this.direction == "N") this.direction = "E";
+        else if (this.direction == "E") this.direction = "S";
+        else if (this.direction == "S") this.direction = "W";
+        else if (this.direction == "W") this.direction = "N";
+        break;
+      case "u":
+        if (
+          this.direction == "N" ||
+          this.direction == "S" ||
+          this.direction == "E" ||
+          this.direction == "W"
+        )
+          this.direction = "UP";
+        else if (this.direction == "DOWN") this.direction = "DOWN";
+        break;
+      case "d":
+        break;
+
+      default:
+        console.error("paramaters passed in turn function are wrong");
+        break;
+    }
+  }
 }
